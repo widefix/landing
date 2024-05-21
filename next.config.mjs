8870e5dd2707dc676ada564e6bf4f319fea644e3
Vladislav Sokov <vladislav.sokov.92@gmail.com>
2024-05-21 13:12:51 +0300

    fix permanent redirect from the old showcases URLs

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/showcase-1-stripe-integration',
-        destination: '/showcases/stripe-integration',
-        permanent: true
-      },
-      {
-        source: '/showcase-2-prevent-account-sharing',
-        destination: '/showcases/prevent-account-sharing',
-        permanent: true
-      }
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
