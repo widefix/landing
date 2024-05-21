c9036292084cd04fc9e0d11be146e11abfc8f5ac
Vladislav Sokov <vladislav.sokov.92@gmail.com>
2024-05-21 11:40:22 +0300

    add permanent redirect from the old showcases URLs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/showcase-1-stripe-integration',
+        destination: '/showcases/stripe-integration',
+        permanent: true
+      },
+      {
+        source: '/showcase-2-prevent-account-sharing',
+        destination: '/showcases/prevent-account-sharing',
+        permanent: true
+      }
+    ];
+  },
+};
 
 export default nextConfig;
