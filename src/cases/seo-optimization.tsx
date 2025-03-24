3b805ee1db2ef1806124fb3deb8e8675c949b5cc
Andrei Kaleshka <ka8725@gmail.com>
2025-03-24 22:40:06 +0100

    seo optimization showcase

---
+++ src/cases/seo-optimization.tsx
@@ -0,0 +1,106 @@
+import { CategoryName } from "@/categories";
+import { ResultBoxColor, Showcase, SwiperSlideColor } from "@/enums";
+
+const result: Showcase = {
+  slug: "seo-optimization",
+  category: CategoryName.optimisations,
+  preview: {
+    companyName: "Worship Online",
+    solution: "SEO Optimization",
+    results: "Our SEO optimization was a game-changer for the client.",
+    wrapperColor: SwiperSlideColor.blue,
+    buttonColor: SwiperSlideColor.blue,
+    companyImageSrc: "/img/showcases/clients/wo.svg",
+    url: ''
+  },
+  body: {
+    bannerTopTitle: (
+      <h1>
+        SEO optimization as a game-<span className="oval">changer</span>
+      </h1>
+    ),
+    bannerTopImageSrc: "/img/showcases/seo-results.png",
+    bannerTopImageWebpSrc: "/img/showcases/seo-results.webp",
+    description: "SEO optimization was a game-changer for the client",
+    descriptionText: (
+      <p>
+        Our SEO optimization was a game-changer for the client. It resulted in a <strong>4000% increase in organic traffic</strong> and a <strong>300% increase in conversions</strong>.
+      </p>
+    ),
+    detailsTitle: 'SEO Optimization',
+    detailsText: (
+      <p>
+        Our SEO optimization was a game-changer for the client. It resulted in a <strong>4000% increase in organic traffic</strong> and a <strong>300% increase in conversions</strong>.
+      </p>
+    ),
+    detailsImageSrc: "/img/showcases/case/seo-problem.svg",
+    bannerProblemWebp: "/img/showcases/case/seo-wo.webp",
+    bannerProblemPng: "/img/showcases/case/seo-wo.png",
+    problemText: (
+      <p>
+        The client had a great product but was struggling to attract new customers. Their website was not ranking well in search engine results, and they were missing out on a lot of potential business.
+      </p>
+    ),
+    solutionFirstText: (
+      <p>
+        We conducted a thorough SEO audit of the client's website and identified a number of issues that were holding them back. We then implemented a series of changes to improve their search engine rankings and attract more organic traffic.
+      </p>
+    ),
+    solutionSecondText: (
+      <p>
+        Year-to-year results were dramatic. The client saw a <strong>4000% increase in organic traffic</strong> and a <strong>300% increase in conversions</strong>. They were able to attract new customers and grow their business like never before.
+      </p>
+    ),
+    bannerSolutionWebp: "/img/showcases/seo-results.png",
+    bannerSolutionPng: "/img/showcases/seo-results.png",
+    resultBoxes: [
+      {
+        color: ResultBoxColor.lightBlue,
+        imageSrc: "/img/showcases/case/icons/stock.svg",
+        message: "Organic traffic increase",
+        number: "+4000%"
+      },
+      {
+        color: ResultBoxColor.lightGreen,
+        imageSrc: "/img/showcases/case/icons/money.svg",
+        message: "Conversions increase",
+        number: "+300%"
+      },
+      {
+        color: ResultBoxColor.darkBlue,
+        imageSrc: "/img/showcases/case/icons/stock.svg",
+        message: "Position in search results",
+        number: "+40%"
+      },
+      {
+        color: ResultBoxColor.green,
+        imageSrc: "/img/showcases/case/icons/user.svg",
+        message: "Impressions increase",
+        number: "+1000%"
+      }
+    ],
+    resultText: (
+      <p>
+        Our SEO optimization was a game-changer for the client. It resulted in a <strong>4000% increase in organic traffic</strong> and a <strong>300% increase in conversions</strong>.
+      </p>
+    ),
+    helpTitle: "Need help with SEO optimization?",
+    related: [
+      {
+        companyName: "Worship Online",
+        solution: "Learn how to improve SEO",
+        results: "Recommendations on optimizing Web apps for SEO.",
+        wrapperColor: SwiperSlideColor.yellow,
+        buttonColor: SwiperSlideColor.yellow,
+        companyImageSrc: "/img/showcases/clients/wo.svg",
+        url: "https://blog.widefix.com/improve-nextjs-application-performance/"
+      }
+    ]
+  },
+  metadata: {
+    title: "SEO Optimization - WideFix",
+    description: "See how our SEO optimization was a game-changer for the client, resulting in a 4000% increase in organic traffic and a 300% increase in conversions."
+  }
+}
+
+export default result;
--- src/showcases.tsx
+++ src/showcases.tsx
@@ -1,81 +1,7 @@
 import { ReactNode } from 'react';
 
-export enum CategoryName {
-  systemsIntegrations = "systems-integrations",
-  optimisations = "optimisations",
-  devDesign = "dev-design",
-  devops = "devops",
-  qualityAssurance = "quality-assurance"
-}
-
-export enum ResultBoxColor {
-  green = "green",
-  blue = "blue",
-  red = "red",
-  orange = "orange",
-  lightBlue = "light-blue",
-  lightGreen = "light-green",
-  darkBlue = "dark-blue"
-}
-
-export enum SwiperSlideColor {
-  purple = "purple",
-  orange = "orange",
-  green = "green",
-  blue = "blue",
-  yellow = "yellow",
-  sea = "sea"
-}
-
-export interface ResultBox {
-  color: ResultBoxColor;
-  imageSrc: string;
-  message: string;
-  number: string;
-}
-
-export interface ShowcasePreview {
-  companyName: string;
-  solution: string;
-  results: string;
-  wrapperColor: SwiperSlideColor;
-  buttonColor: SwiperSlideColor;
-  companyImageSrc: string;
-  url: string;
-}
-
-export interface ShowcaseBody {
-  bannerTopTitle: ReactNode;
-  bannerTopImageSrc: string;
-  bannerTopImageWebpSrc?: string;
-  description: string;
-  descriptionText: ReactNode;
-  detailsTitle: string;
-  detailsText: ReactNode;
-  detailsImageSrc: string;
-  problemText: ReactNode;
-  solutionFirstText: ReactNode;
-  solutionSecondText: ReactNode;
-  resultBoxes: ResultBox[];
-  resultText: ReactNode;
-  helpTitle: string;
-  related?: ShowcasePreview[];
-  bannerProblemWebp?: string;
-  bannerProblemPng?: string;
-  bannerSolutionWebp?: string;
-  bannerSolutionPng?: string;
-}
-
-export interface Showcase {
-  category: CategoryName;
-  slug: string;
-  preview: ShowcasePreview;
-  body: ShowcaseBody;
-  metadata: {
-    title: string;
-    description: string;
-  }
-}
+import SeoOptimization from '@/cases/seo-optimization';
+import { CategoryName, ResultBoxColor, Showcase, SwiperSlideColor } from './enums';
 
 const showcases: Showcase[] = [
   {
@@ -375,6 +301,7 @@ const showcases: Showcase[] = [
       description: "See how we built and launched a mobile app with an advanced feature that allowed our client to continue growing."
     }
   },
+  SeoOptimization
 ];
 
 export default showcases;
