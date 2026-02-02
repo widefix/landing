69f458c5313e2e7cc77fe4711f2e7dc823491260
Andrei Kaleshka <ka8725@gmail.com>
2026-02-02 21:03:24 +0100

    Selected reviews

--- src/components/ClutchWidget.tsx
+++ src/components/ClutchWidget.tsx
@@ -4,7 +4,7 @@ export default function ClutchWidget() {
   return (
     <div
       dangerouslySetInnerHTML={{
-        __html: `<script type="text/javascript" src="https://widget.clutch.co/static/js/widget.js"></script><div class="clutch-widget" data-url="https://widget.clutch.co" data-widget-type="12" data-height="375" data-nofollow="false" data-expandifr="true" data-scale="100" data-reviews="225249,177597,167361,155930,29711,80319,67459" data-clutchcompany-id="1340654"></div>`
+        __html: `<script type="text/javascript" src="https://widget.clutch.co/static/js/widget.js"></script> <div class="clutch-widget" data-url="https://widget.clutch.co" data-widget-type="12" data-height="375" data-nofollow="false" data-expandifr="true" data-scale="100" data-scale="100" data-reviews="431257,428739,419114,225249,177597,29711,80319" data-clutchcompany-id="1340654"></div>`
       }}
     />
   );
